009d088a4b29adee39916abbd78fecd5083e3466
eri1on <et58394@ubt-uni.net>
2023-05-04 16:56:14 +0200

    changed the redirection to the '/MyDiet' once user login

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -26,7 +26,7 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = RouteServiceProvider::HOME;
+    protected $redirectTo = '/MyDiet';
 
     /**
      * Create a new controller instance.
