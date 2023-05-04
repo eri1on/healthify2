dd3d3deef64dfd6614ed5de6157c956547517285
eri1on <et58394@ubt-uni.net>
2023-05-04 17:04:36 +0200

    inside the rues function we must and we have added some new rules when an profileUpdateRequest is made by a user

--- app/Http/Requests/ProfileUpdateRequest.php
+++ app/Http/Requests/ProfileUpdateRequest.php
@@ -18,6 +18,16 @@ class ProfileUpdateRequest extends FormRequest
         return [
             'name' => ['string', 'max:255'],
             'email' => ['email', 'max:255', Rule::unique(User::class)->ignore($this->user()->id)],
+            'password' => ['nullable', 'string', 'min:8', 'confirmed'],
+            'age' => ['required', 'integer', 'min:0', 'max:150'],
+            'gender' => ['required', 'string', 'in:male,female'],
+            'height' => ['required', 'numeric', 'min:0', 'max:300'],
+            'weight' => ['required', 'numeric', 'min:0', 'max:1000'],
+            'goal' => ['required', 'string', 'in:lose_weight,gain_weight'],
+            'activity' => ['required', 'string', 'in:low_activity,high_activity'],
         ];
     }
+
+ 
+
 }
