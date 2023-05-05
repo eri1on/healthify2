07ec271bebdcdaa83a50159d8126c5a6c78bae75
eri1on <et58394@ubt-uni.net>
2023-05-05 19:21:17 +0200

    added the edit,update and destroy methods in the userController file

--- app/Http/Controllers/userController.php
+++ app/Http/Controllers/userController.php
@@ -18,6 +18,44 @@ class userController extends Controller
 
     }
 
+    
+    //This method will fetch the user's informations from the database and return the 'admin-user-edit' view.
+    public function edit($id){
+
+        $user=User::findOrFail($id);
+
+        return view('admin-user-edit',compact('user'));
+    }
+
+
+    // Update method
+
+    public function update(Request $request ,$id){
+        $user=User::findOrFail($id);
+
+        $user->name=$request->input('name'); // update the name.
+        $user->email=$request->input('email'); //UPDATE the Emaiil
+        $user->age=$request->input('age');
+        $user->gender=$request->input('gender');
+        $user->height=$request->input('height');
+        $user->weight=$request->input('weight');
+        $user->goal=$request->input('goal');
+        $user->activity=$request->input('activity');
+        $user->is_admin=$request->input('is_admin');
+        $user->save();  // save changes
+
+        return redirect()->route('userinfoshow')->with('success', 'User updated Successfully.');
+    }
+
+    
+    // Destroy or delete Method
+
+    public function destroy($id){
+        $user=User::findOrFail($id);
+        $user->delete();   //delete the user.
+        return redirect()->route('userinfoshow')->with('success','User Deleted Successfully');
+    }
+
 
 
 }
