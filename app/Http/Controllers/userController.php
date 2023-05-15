c44021654801f240e343915c60e9996b5c3ce46c
eri1on <et58394@ubt-uni.net>
2023-05-15 23:39:37 +0200

    changed validation rules in backend  when a profileUpdate is going to be made from an admin

--- app/Http/Controllers/userController.php
+++ app/Http/Controllers/userController.php
@@ -6,6 +6,9 @@ use Illuminate\Http\Request;
 
 use App\Models\User;
 
+use Illuminate\Validation\Rule;
+use Illuminate\Support\Facades\Hash;
+
 class userController extends Controller
 {
     //
@@ -31,18 +34,40 @@ class userController extends Controller
     // Update method
 
     public function update(Request $request ,$id){
+
+        $validatedData = $request->validate([
+            'name' => ['required', 'string', 'max:255',                            'regex:/^[a-zA-z]{2,}/'],
+            'email' => ['email', 'max:255', Rule::unique(User::class)->ignore($id)],
+            'password' => ['nullable', 'string', 'min:8', 'confirmed',              'regex:/^.{8,}$/'],
+            'age' => ['required', 'numeric', 'min:18',                              'regex:/^\d{2}$/'],
+            'gender' => ['required', 'in:male,female',                              'regex:/^(male|female)$/i'],
+            'height' => ['required', 'numeric', 'min:100', 'max:350',                 'regex:/^(?:[1-2]\d{2}|3[0-4]\d|350)$/'],
+            'weight' => ['required', 'numeric', 'min:30', 'max:400',                 'regex:/^([3-9][0-9]{1}|[1-9][0-9]{2}|400)$/'],
+            'goal' => ['required', 'in:lose_weight,gain_weight',                    'regex:/^(lose_weight|gain_weight)$/i'],
+            'activity' => ['required', 'in:high_activity,low_activity',             'regex:/^(high_activity|low_activity)$/i'],
+        ]);
+        
+
+
+
         $user=User::findOrFail($id);
 
-        $user->name=$request->input('name'); // update the name.
-        $user->email=$request->input('email'); //UPDATE the Emaiil
-        $user->age=$request->input('age');
-        $user->gender=$request->input('gender');
-        $user->height=$request->input('height');
-        $user->weight=$request->input('weight');
-        $user->goal=$request->input('goal');
-        $user->activity=$request->input('activity');
-        $user->is_admin=$request->input('is_admin');
-        $user->save();  // save changes
+        $user->name = $validatedData['name'];
+        $user->email = $validatedData['email'];
+        $user->age = $validatedData['age'];
+        $user->gender = $validatedData['gender'];
+        $user->height = $validatedData['height'];
+        $user->weight = $validatedData['weight'];
+        $user->goal = $validatedData['goal'];
+        $user->activity = $validatedData['activity'];
+        $user->is_admin = $request->input('is_admin', false);
+        if ($request->filled('password')) {
+            $user->password = Hash::make($request->input('password'));
+        }else {
+            // If the password fields are not filled, use the user's current password
+            $user->password = $user->getOriginal('password');
+        }
+        $user->save(); // save changes
 
         return redirect()->route('userinfoshow')->with('success', 'User updated Successfully.');
     }
