9a4c3b1c9573017fed7dbd46f97cc3c786c8d75b
eri1on <et58394@ubt-uni.net>
2023-05-15 23:37:30 +0200

    added an if statement to get the original user password and send it to the database if the password input for profile update is not filled

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -44,18 +44,24 @@ class ProfileController extends Controller
      */
     public function update(ProfileUpdateRequest $request): RedirectResponse
     {
+    
         $user = $request->user();
         $user->fill($request->validated());
     
         // Hash the password if it is not empty
         if ($request->filled('password')) {
             $user->password = Hash::make($request->input('password'));
+        }else {
+            // If the password fields are not filled, use the user's current password
+            $user->password = $user->getOriginal('password');
         }
     
         if ($user->isDirty('email')) {
             $user->email_verified_at = null;
         }
-    
+       
+     
+        
         $user->save();
     
         return Redirect::route('userprofileshow')->with('success', 'Profile updated successfully');
