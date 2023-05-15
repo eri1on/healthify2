8d880bd379e30da7ea3a067a8b97a4121b533b0e
eri1on <et58394@ubt-uni.net>
2023-05-15 17:22:00 +0200

    added some validation rules using javascript for login form

--- public/js-Validations/login-validation.js
+++ public/js-Validations/login-validation.js
@@ -1,19 +1,22 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const form=document.querySelector('form');
-    const emailInput = document.querySelector('#email');
-    const passwordInput=document.querySelector('#password');
-  
-    form.addEventListener('submit',function(event){
-        if(!emailInput.value){
-            alert('Please enter your email');
-            event.preventDefault();
-        }
-  
-        if(!passwordInput.value){
-          alert('Please enter a password');
-          event.preventDefault();
-        }
-    });
-  });
-  
+function loginValidation(){
 
+
+var emailInput=document.getElementById('email').value;
+var passwordInput=document.getElementById('password').value;
+
+var errorDiv=document.getElementById('error-div');
+
+
+if(emailInput===""){
+  errorDiv.innerText="Please enter your email address!";
+  document.getElementById('email').focus();
+  return false;
+}
+
+if(passwordInput===""){
+  errorDiv.innerText="Please enter your password!";
+  document.getElementById('password').focus();
+  return false;
+}
+
+}
