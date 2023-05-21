41179e3aa22b8b9af929f679720cb7e486a721e0
eri1on <et58394@ubt-uni.net>
2023-05-21 19:43:56 +0200

    created a new dashbord page

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,41 @@
+@extends('layouts.app')
+@section('content')
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="/css/dashboard.css">
+</head>
+<body>
+    @if(session('error'))
+    <div class="alert alert-danger">
+        {{ session('error') }}
+    </div>
+@endif
+
+   
+    @php
+    $user = auth()->user();
+@endphp
+@if($user && ($user->is_superadmin || $user->is_admin))
+
+<div class="top-div">
+<button id="nav-btn" class="btn btn-info" ><a href="{{route('userinfoshow')}}">USERS</a></button>
+<br>
+<button id="btn-addFood"class="btn btn-info" ><a href="{{route('showCreateFoodForm')}}">ADD FOOD</a></button>
+</div>
+@else
+<div class="alert alert-danger">
+    Unauthorized access! Only admins and superadmins can access this page.
+</div>
+
+@endif
+</body>
+</html>
+
+@endsection
