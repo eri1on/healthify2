54dc23a062ed33f9d329fe006fc853951be27f02
eri1on <et58394@ubt-uni.net>
2023-05-21 19:45:54 +0200

    in Foods Model i added a line of code that tells laravel that the column of 'if' id the foods table is 'foods_id'

--- app/Models/Foods.php
+++ app/Models/Foods.php
@@ -7,7 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Foods extends Model
 {
-    Protected $table='foods';
+    protected $table='foods';
+ protected $primaryKey = 'food_id';//By default, Laravel assumes that the primary key column in the table is named id, and that way we added this line of code 
     use HasFactory;
     protected $fillable = [
         'nameOfFood',
