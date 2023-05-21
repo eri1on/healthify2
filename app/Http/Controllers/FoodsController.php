32cdcf31f24d89d506b611fe6c5ef06ac9146d76
eri1on <et58394@ubt-uni.net>
2023-05-21 13:55:01 +0200

    i added a method which will allow user to go to the 'add-food' view if the user is admin or superadmin

--- app/Http/Controllers/FoodsController.php
+++ app/Http/Controllers/FoodsController.php
@@ -10,10 +10,21 @@ use App\Models\User;
 
 class FoodsController extends Controller
 {
+
+    //This method is created to only allow admins and superadmins to be able to access the 'add-food'  view
+     public function showForm(){
+        $user=Auth::user();
+        if ($user && ($user->is_admin || $user->is_superadmin)){
+            return view('add-food');
+        }else{
+            return redirect('/')->with('You are not allowed to access this page!');
+        }
+     }
    
    
     public function store(Request $request){
         $user = auth()->user();
+
 
         $validatedData=$request->validate([
          'nameOfFood'=>['required','string','max:255', 'regex:/^[A-Za-z]{2,}$/'],
@@ -26,15 +37,16 @@ class FoodsController extends Controller
         ]); 
         if(Auth::user() && (Auth::user()->is_superadmin || Auth::user()->is_admin)){
         Foods::create($validatedData);// Create a new row in the 'foods' table with the validated data
-        
+
         return redirect()->back()->with('success','Food added successfully!');
     }else{
         return redirect()->back()->with('error', 'Unauthorized action!');
     }
-    
+
     }
 
 
+    //this method displayes all the foods in the view called 'select-foods.blade.php'
     public function getFoods(){
         $user=Auth::user();
 
