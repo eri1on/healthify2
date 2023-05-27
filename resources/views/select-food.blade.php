06d35f4f3483ed293baea255eaa9c821920adbb9
eri1on <et58394@ubt-uni.net>
2023-05-27 17:43:25 +0200

    new html structure for select-food.blade.php created

--- resources/views/select-food.blade.php
+++ resources/views/select-food.blade.php
@@ -1,75 +1,70 @@
-
 @extends('layouts.app')
 @section('content')
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-    <link rel="stylesheet" href={{asset('css/select-food.css')}}>
-</head>
-<body>
-    <section class="food-section">
-        <div class="food-container">
-          <h2 class="food-title">Your Diet Starts Here:</h2>
-          <p class="food-paragraph">! Please choose the foods carefully and note that the food you select for your diet will be the food for 1 week and you cannot change it.</p>
-          <form id="food-form" action="/submit" method="post">
-          <table class="food-table">
-            <thead>
-              <tr>
-                <th>Category</th>
-                <th>Food Item</th>
-                <th>Day Of The Week</th>
-                <th>Meal Type</th>
-                <th>Select</th>
-              </tr>
-            </thead>
-            <tbody>
-           @foreach ($allFoods as $food)
-           <tr>
-            <td>{{$food->category}}</td>
-            <td>{{$food->nameOfFood}}</td>
-                <td>
-                    <select name="day">
-                        <option value="Monday">Monday</option>
-                        <option value="Tuesday">Tuesday</option>
-                        <option value="Wednesday">Wednesday</option>
-                        <option value="Thursday">Thursday</option>
-                        <option value="Friday">Friday</option>
-                        <option value="Saturday">Saturday</option>
-                        <option value="Sunday">Sunday</option>
-                      </select>
 
-                </td>
 
-                <td>
-                 <select name="mealType">
-                    <option value="Breakfast">Breakfast</option>
-                    <option value="Lunch">Lunch</option>
-                    <option value="Dinner">Dinner</option>
-                    <option value="Snacks">Snacks</option>
-                   
-                 </select>
 
-                </td>
+<link rel="stylesheet" href="{{asset('css/select-food.css')}}">
+<div class="top-div">
+    <h5>Create Your Diet</h5>
+<form  id="diet-form" method="POST" action="{{ route('startUserDiet') }}" onsubmit="return valid();">
+   
+    @csrf
+    <div id="foods-container">
+        <h4><span style="color:red; font-weight:500">Note!</span>You can choose foods for you diet only once a week. After 1 week you can make changes to you diet!</h4>
+        <p>So please do this proccess carefully </p>
+       
+        <div class="food-row">
+            <label>Food:</label>
+            <select class="select-1"name="foods[]" >
+                <!-- Render options dynamically from the foods table in the database -->
+                @foreach ($foods as $food)
+                <option class="options" value="{{ $food->food_id }}">{{ $food->nameOfFood }}</option>
+                
+                @endforeach
+                
+            </select>
+           
+            <label>Meal Type:</label>
+            <select class="select-1"name="meal_types[]" >
+               
+                <option class="options"value="breakfast">Breakfast</option>
+                <option class="options"value="lunch">Lunch</option>
+                <option class="options"value="dinner">Dinner</option>
+                <option class="options"value="snacks">Snacks</option>
+                
+            </select>
             
-            <td>
-                <input type="checkbox" name="food[]" value="{{ $food->id }}">
-            </td>
-          </tr>
-          @endforeach
-            </tbody>
-          </table>
-          <div class="field-div-btn">
-            <button type="submit" class="btn btn-primary">
-                {{ __('Create My Diet') }}
-            </button>
+            <label>Day of Week:</label>
+            <select class="select-1" name="days[]" >
+                
+                <option class="options"value="monday">Monday</option>
+                <option class="options"value="tuesday">Tuesday</option>
+                <option class="options"value="wednesday">Wednesday</option>
+                <option class="options"value="thursday">Thursday</option>
+                <option class="options"value="friday">Friday</option>
+                <option class="options"value="saturday">Saturday</option>
+                <option class="options"value="sunday">Sunday</option>
+             
+                
+            </select>
+            
         </div>
-          </form>
-        </div>
-      </section>
-</body>
-</html>
+        
+    </div>
+<div class="buttons">
+    <button type="button" id="add-food-row">+</button>
+    <button  class="btn-create"type="submit">Create My Diet</button></div>
+    
+</form>
+<div class="errorDiv">
+    <span id="error-message"  style="color:red; font-weight:600"></span><br>
+    <span id ="errorDiv" style="color:brown; font-weight:600"></span>
+</div>
+
+</div>
+
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<script src="{{ asset('js-Validations/select-food-validation.js') }}"></script>
+
 @endsection
+
