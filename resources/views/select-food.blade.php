f16da47e285d4079a64ce2120a349bc61091ad7f
eri1on <et58394@ubt-uni.net>
2023-05-31 23:05:48 +0200

    added the '@error'     session to display error messags for each field

--- resources/views/select-food.blade.php
+++ resources/views/select-food.blade.php
@@ -5,13 +5,14 @@
 
 <link rel="stylesheet" href="{{asset('css/select-food.css')}}">
 <div class="top-div">
-    <h5>Create Your Diet</h5>
-<form  id="diet-form" method="POST" action="{{ route('startUserDiet') }}" onsubmit="return valid();">
+   
+<form  id="diet-form" method="POST" action="{{ route('startUserDiet') }}" onsubmit="return valid();" >
    
     @csrf
     <div id="foods-container">
-        <h4><span style="color:red; font-weight:500">Note!</span>You can choose foods for you diet only once a week. After 1 week you can make changes to you diet!</h4>
-        <p>So please do this proccess carefully </p>
+        
+       
+     
        
         <div class="food-row">
             <label>Food:</label>
@@ -23,7 +24,10 @@
                 @endforeach
                 
             </select>
-           
+            @error('foods')
+                <span class="error-message" style="color: red; font-weight: 600">{{ $message }}</span>
+                @enderror
+
             <label>Meal Type:</label>
             <select class="select-1"name="meal_types[]" >
                
@@ -31,9 +35,12 @@
                 <option class="options"value="lunch">Lunch</option>
                 <option class="options"value="dinner">Dinner</option>
                 <option class="options"value="snacks">Snacks</option>
+              
                 
             </select>
-            
+             @error('meal_types')
+                <span class="error-message" style="color: red; font-weight: 600">{{ $message }}</span>
+                @enderror
             <label>Day of Week:</label>
             <select class="select-1" name="days[]" >
                 
@@ -44,25 +51,32 @@
                 <option class="options"value="friday">Friday</option>
                 <option class="options"value="saturday">Saturday</option>
                 <option class="options"value="sunday">Sunday</option>
-             
                 
             </select>
+            @error('days')
+            <span class="error-message" style="color: red; font-weight: 600">{{ $message }}</span>
+            @enderror
             
         </div>
         
     </div>
 <div class="buttons">
-    <button type="button" id="add-food-row">+</button>
+     <button type="button" id="add-food-row">+</button>
+     <img src="../img/sandwich.png"  style="width:80px;">
     <button  class="btn-create"type="submit">Create My Diet</button></div>
     
 </form>
+
 <div class="errorDiv">
     <span id="error-message"  style="color:red; font-weight:600"></span><br>
     <span id ="errorDiv" style="color:brown; font-weight:600"></span>
 </div>
 
 </div>
-
+<ul>
+<li><p style="font-size:large; color:midnightblue; text-transform: uppercase;">Note! you can only choose between 10 to 20 foods, Otherwise you cannot create your Diet</p></li>
+<li><p style="color:midnightblue; text-transform: uppercase; ">Once you submit the diet you cannot change the foods before 1 week has passed,<br>after 1 week you can make changes to you diet</p></li>
+</ul>
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 <script src="{{ asset('js-Validations/select-food-validation.js') }}"></script>
 
