3943ea19798481577e1fdbfeb20be07f1aa73b0c
eri1on <et58394@ubt-uni.net>
2023-05-31 23:01:55 +0200

    ReCreated the user_diet and user_meal_plan migrations file and added some changes to the foreign keys (onDelete and onUpdate  cascade)

---
+++ database/migrations/2023_05_28_155131_create_user_meal_plan-table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('user_meal_plan', function (Blueprint $table) {
+            $table->id('meal_plan_id');
+            $table->string('day_of_week', 255); 
+            $table->string('mealType',9);
+            $table->unsignedBigInteger('fk_signUp_id');
+            $table->unsignedBigInteger('fk_food_id');
+            $table->unsignedBigInteger('fk_diet_id');
+            $table->float('personalized_calories');
+            $table->timestamps();
+
+            $table->foreign('fk_signUp_id')->references('id')->on('users')->onDelete('cascade')->onUpdate('cascade');
+            $table->foreign('fk_food_id')->references('food_id')->on('foods')->onDelete('cascade')->onUpdate('cascade');
+            $table->foreign('fk_diet_id')->references('diet_id')->on('user_diet')->onDelete('cascade')->onUpdate('cascade');
+        
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('user_meal_plan');
+    }
+};
