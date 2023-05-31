e870daeb88ae3e13360f8aa111d69f73155dd3bc
eri1on <et58394@ubt-uni.net>
2023-05-31 23:11:44 +0200

    html structure created and displayed the  diet data  of the  current authenticated user

---
+++ resources/views/user-diet.blade.php
@@ -0,0 +1,67 @@
+
+@extends('layouts.app')
+@section('content')
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="{{asset('css/user-diet.css')}}">
+</head>
+<body>
+    <div class="table-container">
+    <table>
+        <thead style="color:#5C8984;  position: sticky;">
+ <th>Category</th>
+<th>Food Item</th>
+<th>Day Of Week</th>
+<th>Meal Type</th>
+<th>Vitamin</th>
+<th>Proteins</th>
+<th>Carbohydrates</th>
+<th>Daily Calories</th>
+
+        </thead>
+
+        <tbody>
+            @php
+            $previousDay = null;
+        @endphp
+
+
+            @foreach($diet as $userDiet)
+         
+            <tr style="text-transform: uppercase;">
+                <td>{{ $userDiet->food->category }}</td>
+                <td>{{ $userDiet->food->nameOfFood }}</td>
+                <td>{{ $userDiet->day_of_week }}</td>
+                <td>{{ $userDiet->mealType }}</td>
+                <td>{{ $userDiet->food->vitamins }}</td>
+                <td>{{ $userDiet->food->proteins }}</td>
+                <td>{{ $userDiet->food->carbohydrates }}</td>
+                @if ($userDiet->day_of_week !== $previousDay)
+                <td style="color:midnightblue;font-size:large">{{ $userDiet->personalized_calories }}
+                    <span style="color:green; font-weight:700; font-size:small; text-transform: uppercase;">({{$userDiet->day_of_week}})</span><br>  <br><br></td>
+                  
+                @endif
+                
+            </tr>
+       
+        @php
+            $previousDay = $userDiet->day_of_week;
+        @endphp
+      
+            @endforeach
+
+        </tbody>
+
+
+    </table>
+    </div>
+</body>
+</html>
+
+@endsection
