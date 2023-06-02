24423b835624a1ce2e2684257f46e90c127029a6
eri1on <et58394@ubt-uni.net>
2023-06-02 15:30:03 +0200

    'index2' method changed -> to check if there is any active diet of the user in order to call the 'updateDiet' view . and defined the updateDiet method.

--- app/Http/Controllers/DietController.php
+++ app/Http/Controllers/DietController.php
@@ -99,4 +99,94 @@ function calculatePersonalizedCalories($user,$TCalories)
 
     return $TCalories;
 }
+
+
+
+// --------------------------------- This Method will allow users to update their Diet--------------------------
+
+
+public function index2()
+{
+    if (Auth::user()) {
+        $user = Auth::user();
+        
+        // Retrieve the user's current diet based on the current week
+        $diet = UserDiet::where('fk_signUp_id', $user->id)
+            ->whereDate('week_start_date', '<=', now())
+            ->whereDate('week_end_date', '>=', now())
+            ->first();
+
+        if ($diet) {
+            // Retrieve the user's meal plans for the current diet
+            $mealPlans = UserMealPlan::where('fk_diet_id', $diet->diet_id) ->get();
+
+            // Retrieve all available foods for selection
+            $foods = Foods::all();
+
+            return view('updateDiet', compact('mealPlans', 'foods'));
+        } else {
+            // No active diet found for the user
+            return redirect()->route('showDiet')->with('error', 'No active diet found.');
+        }
+    }
+}
+
+
+public function updateDiet(Request $request)
+{
+    // Validate the form data
+    $request->validate([
+        'foods' => 'required|array|min:10|max:20',
+        'meal_types' => 'required|array|min:10|max:20',
+        'meal_types.*' => 'required|in:breakfast,lunch,dinner,snacks',
+        'days' => 'required|array|min:10|max:20',
+        'days.*' => 'required|in:monday,tuesday,wednesday,thursday,friday,saturday,sunday',
+    ]);
+
+    $foods = $request->input('foods');
+    $mealTypes = $request->input('meal_types');
+    $days = $request->input('days');
+
+    // Retrieve the user's current diet based on the current week
+    $user = Auth::user();
+    $diet = UserDiet::where('fk_signUp_id', $user->id)
+        ->whereDate('week_start_date', '<=', now())
+        ->whereDate('week_end_date', '>=', now())
+        ->first();
+
+    if ($diet) {
+        // Delete the existing meal plans for the current diet
+        UserMealPlan::where('fk_diet_id', $diet->diet_id)->delete();
+
+        $totalCalories = 0;
+
+
+        foreach ($foods as $index => $foodId) {
+            $food = Foods::find($foodId);
+            $totalCalories += $food->calories;
+
+            $mealPlan = new UserMealPlan();
+            $mealPlan->day_of_week = $days[$index];
+            $mealPlan->mealType = $mealTypes[$index];
+            $mealPlan->fk_signUp_id = auth()->user()->id;
+            $mealPlan->fk_food_id = $foodId;
+            $mealPlan->fk_diet_id = $diet->diet_id;
+            $mealPlan->personalized_calories = $this->calculatePersonalizedCalories($user, $totalCalories);
+            $mealPlan->save();
+        }
+        $diet->week_start_date = now();
+        $diet->week_end_date = now()->addDays(7);
+        $diet->save();
+
+        // Redirect or show a success message
+        return redirect()->route('showDiet')->with('success', 'Your diet has been updated successfully.');
+    } else {
+        // No active diet found for the user
+        return redirect()->route('showDiet')->with('error', 'No active diet found.');
+    }
+}
+
+
+
+
 }
