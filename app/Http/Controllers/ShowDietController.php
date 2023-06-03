b92ccf727fcfc7b21a297c767b4ca7835f14c59b
eri1on <et58394@ubt-uni.net>
2023-06-03 16:09:24 +0200

    showDiet method an inside the compact() method sends  two variables 'diet' and 'alldiet'   , send them to the view called'user-diet'

--- app/Http/Controllers/ShowDietController.php
+++ app/Http/Controllers/ShowDietController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Models\userMealPlan;
 use App\Models\Foods;
+use App\Models\userDiet;
 
 use App\Models\User;
 class ShowDietController extends Controller
@@ -16,16 +17,17 @@ class ShowDietController extends Controller
     public function showDiet(){
   
         $user=Auth::user();
+        $allDiet=userDiet::where('fk_signUp_id',$user->id)->first();
 
        if(!$user){
-        return redirect->route('/');
+        return redirect()->route('/');
        }else{
          $diet = UserMealPlan::with('food')
             ->where('fk_signup_id', $user->id)
             ->orderByRaw("FIELD(day_of_week, 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday')")
             ->get();
             
-             return view('user-diet',compact('diet'));
+             return view('user-diet',compact('diet','allDiet'));
 
        }
         
