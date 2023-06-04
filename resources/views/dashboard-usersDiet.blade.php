19b1336b5b0ecf5e16d176fedb7b72b4160b9d6f
eri1on <et58394@ubt-uni.net>
2023-06-04 21:53:43 +0200

    we now check if the user is admin or superadmin in order to display the table in the view'dashboard-usersDiet'

--- resources/views/dashboard-usersDiet.blade.php
+++ resources/views/dashboard-usersDiet.blade.php
@@ -8,29 +8,37 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
-
+<link rel="stylesheet" href="{{asset('/css/dashboard-usersDiet.css')}}">
 </head>
 <body>
-
-
+    @php
+    $user = auth()->user();
+@endphp
+@if($user && ($user->is_superadmin || $user->is_admin))
+<div class="top-div">
     <table>
 
 <thead>
+   
     <th>SignUp-ID</th>
     <th>Diet-ID</th>
+    
 </thead>
 <tbody>
     @foreach($allUsersDiets as $diet)
    
     <tr>
-
+   
     <td>{{$diet->fk_signUp_id}}</td>
+
     <td>{{$diet->diet_id}}</td>
+    <td> <a href="{{route('adminEditDiet',$diet->diet_id)}}"><button class="btn btn-primary">Edit Diet</button> </a>    </td>
     <td>
     <form method="POST" action="{{route('deleteUserDiet',$diet->diet_id)}}" onsubmit="return confirm('Are you sure you want to delete this diet?');">
        @method('DELETE')
        @csrf
      <button type="submit" class="btn btn-danger">Delete Diet</button>
+     
     </form>
 
     </td>
@@ -40,7 +48,10 @@
 
 
     </table>
+</div>
+   
     
+    @endif
 </body>
 </html>
 @endsection
