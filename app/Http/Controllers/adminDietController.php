700e45ed06f04bc50339cdd5cb605a25d70d6bda
eri1on <et58394@ubt-uni.net>
2023-06-04 21:58:31 +0200

    delete method now checks if the user is admin or superadmin in order to alow a user to delete a diet , and created the  'editDiet'   method

--- app/Http/Controllers/adminDietController.php
+++ app/Http/Controllers/adminDietController.php
@@ -6,6 +6,8 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Models\User;
 use App\Models\userDiet;
+use App\Models\userMealPlan;
+use App\Models\Foods;
 
 
 class adminDietController extends Controller
@@ -22,13 +24,16 @@ class adminDietController extends Controller
            return view('dashboard-usersDiet',compact('allUsersDiets'));
 
         }else{
-            return redirect()-back()->with('error','Unauthorized access!');
+            return redirect()->route('myDiet')->with('error','Unauthorized access!');
         }
     }
     
 
 
     public function deleteDiet($id){
+
+       $user=auth()->user();
+       if($user&&(Auth::user()->is_superadmin||Auth::user()->is_admin)){
 
         $findDiet=userDiet::findOrFail($id);
         
@@ -38,10 +43,45 @@ class adminDietController extends Controller
 
             return redirect()->route('admin-diet-dashboard')->with('success','User Diet Deleted Successfully.');
         }else{
-            return redirect()-route('admin-diet-dashboard')->with('error','Something went wrong!');
+            return redirect()->route('admin-diet-dashboard')->with('error','Something went wrong!');
         }
+    }else{
+        return redirect()->route('myDiet')->with('error','Unauthorized access!');
+    }
 
     }
 
+
+
+
+    public function editDiet($id){
+         $user=auth()->user();
+
+         if($user&&(Auth::user()->is_admin||Auth::user()->is_superadmin)){
+
+
+            $findDiet=userDiet::where('diet_id',$id)->first();
+            $foods=Foods::all();
+           
+
+            if($findDiet!==null){
+                $mealPlans=userMealPlan::where('fk_diet_id',$findDiet->diet_id)->get();
+
+                
+                return view('adminDietEdit', compact('findDiet','mealPlans','foods'));
+
+            }else{
+                return redirect()->back()->with('error','Something went wrong!');
+            }
+
+         }else{
+            return redirect()->route('myDiet')->with('error','Unauthorized Action!');
+         }
+
+    }
+
+
+
+    
 
 }
