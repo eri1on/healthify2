f56728749be8d66c860f2abe7260de66d16775fb
eri1on <et58394@ubt-uni.net>
2023-07-16 15:18:58 +0200

    updated (re-Created) the user_meal-plan table

--- database/migrations/2023_07_16_123938_create_user_meal_plan_table.php
+++ database/migrations/2023_07_16_123938_create_user_meal_plan_table.php
@@ -19,7 +19,15 @@ return new class extends Migration
             $table->unsignedBigInteger('fk_food_id');
             $table->unsignedBigInteger('fk_diet_id');
             $table->float('personalized_calories');
+            /*
+            $table->float('portion_size');
+            
             $table->float('personalized_grams');
+            */
+            $table->float('proteinGram');
+
+            $table->float('carbohydratesGram');
+            
             $table->timestamps();
 
             $table->foreign('fk_signUp_id')->references('id')->on('users')->onDelete('cascade')->onUpdate('cascade');
