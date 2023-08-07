aadff5670d33b693f83aaecfd403eefd4cc88fa9
eri1on <et58394@ubt-uni.net>
2023-08-07 18:42:06 +0200

    css style and html structure changes a little

--- resources/views/user-diet.blade.php
+++ resources/views/user-diet.blade.php
@@ -37,18 +37,27 @@ foreach($diet as $userDiet){
 @endphp
 
 
+
+
     <div class="daily-summary">
 
         <div class="card-container">
             <div class="card card-header summary-header">
                 <h5><b>Total Nutritional Values:</b> </h5>
                 <br>
-                <p style="font-style:italic"><strong>Daily Proteins:</strong> {{ $totalProteins }} <span >gram</span></p>
-                <p style="font-style:italic"><strong>Daily Carbohydrates:</strong> {{ $totalCarbohydrates }} <span >gram</span></p>
-                <p style="font-style:italic"><strong>Daily Calories:</strong> {{  $totalCalories }}</p>
+                <p ><strong >Daily Calories:</strong> <span style="color:#080202;font-size:large"> {{  $totalCalories }} <span></p>
+                <p><strong>Daily Proteins:</strong> <span style="color:#080202;font-size:large"> {{ $totalProteins }}  <span style="color:#C51605"> gram </span> </span></p>
+                <p ><strong>Daily Carbohydrates:</strong><span style="color:#080202;font-size:large">  {{ $totalCarbohydrates }}  <span style="color:#C51605">  gram </span></span> </p>
             </div>
         </div>
+
     </div>
+
+
+ 
+    
+
+
 
 
 
@@ -58,18 +67,18 @@ foreach($diet as $userDiet){
        
             <div class="card day-{{ $userDiet->day_of_week }}">
                 <div class="card-header" >
-                <b  style="color:#C51605" >    {{ strtoupper($userDiet->day_of_week) }}</b>
+                <b  style="color:#0E2954" >    {{ strtoupper($userDiet->day_of_week) }}</b>
                    <BR>
-                  <p style="font-style:italic">  {{ ($userDiet->food->nameOfFood) }}<p>
+                  <p class="foodname"style="font-family: sans-serif">  {{strtoupper ($userDiet->food->nameOfFood) }}<p>
                    
                 </div>
                 <div class="card-body">
-                    <p><strong>Category:</strong> {{ $userDiet->food->category }}</p>
-                    <p><strong>Day Of Week:</strong> {{ $userDiet->day_of_week }}</p>
-                    <p><strong>Meal Type:</strong> {{ $userDiet->mealType }}</p>
-                    <p><strong>Vitamin:</strong> {{ $userDiet->food->vitamins }}</p>
-                    <p><strong>Proteins:</strong> {{ $userDiet->food->proteins }} <span >gram</span></p>
-                    <p><strong>Carbohydrates:</strong> {{ $userDiet->food->carbohydrates }} <span >gram</span></p>
+                    <p><strong  style="color:#6C3428">Category:</strong>   <span>  {{  strtoupper($userDiet->food->category )}} </span></p>
+                    <p><strong style="color:#6C3428">Day Of Week:</strong>  <span> {{strtoupper( $userDiet->day_of_week )}} </span></p>
+                    <p><strong style="color:#6C3428">Meal Type:</strong> <span> {{ $userDiet->mealType }}</span> </p>
+                    <p><strong style="color:#6C3428">Vitamin:</strong> <span> {{ strtoupper($userDiet->food->vitamins) }} </span></p>
+                    <p><strong style="color:#6C3428">Proteins:</strong> <span style="color:#C51605"> {{ strtoupper($userDiet->food->proteins) }} gram</span></p>
+                    <p><strong style="color:#6C3428">Carbohydrates:</strong> <span style="color:#C51605">{{ strtoupper($userDiet->food->carbohydrates )}} gram</span> </p>
      
                    
                 </div>
@@ -78,22 +87,22 @@ foreach($diet as $userDiet){
     </div>
 
  
-  <div class="buttons">
-    <p>Before you update your diet, please keep your profile data up to date to get the best results for your diet data.</p>
-    <div class="space-btn">
 
-    <a href="{{route('updateMyDiet')}}"><button id="btn-update" class="btn btn-primary">Edit My Diet</button></a>
-    @if ($allDiet)
-    <form class="delete-form" method="POST" action="{{ route('deleteDiet', $allDiet->diet_id) }}" onsubmit="return confirm('Are you sure you want to delete your diet?');">
-        @method('DELETE')
-        @csrf
-        <button type="submit" class="btn btn-danger">Delete My Diet</button>
-    </form>
-    <a href="{{route('userprofileshow')}}"><button id="btn-profile" class="btn btn-info">Edit Profile</button></a>
-    @endif
+    <div class="buttons">
+        <p>Before you update your diet, please keep your profile data up to date to get the best results for your diet data.</p>
+     <div class="space-btn">
+    
+        <a href="{{route('updateMyDiet')}}"><button id="btn-update" class="btn btn-primary">Edit My Diet</button></a>
+        @if ($allDiet)
+        <form class="delete-form" method="POST" action="{{ route('deleteDiet', $allDiet->diet_id) }}" onsubmit="return confirm('Are you sure you want to delete your diet?');">
+            @method('DELETE')
+            @csrf
+            <button type="submit" class="btn btn-danger">Delete My Diet</button>
+        </form>
+        <a href="{{route('userprofileshow')}}"><button id="btn-profile" class="btn btn-info">Edit Profile</button></a>
+        @endif
+      </div>
     </div>
-  </div>
-   
 
 </body>
 </html>
