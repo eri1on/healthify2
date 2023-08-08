bee87e30c28a014d536d8f249937f68ffbc984db
eri1on <et58394@ubt-uni.net>
2023-08-08 21:04:47 +0200

    contact-us.blade.php    'view' now simply calles the view from the livewire folder

--- resources/views/contact-us.blade.php
+++ resources/views/contact-us.blade.php
@@ -7,24 +7,17 @@
 			href="https://fonts.googleapis.com/css?family=Quicksand&display=swap"
 			rel="stylesheet"
 		/>
-		<meta
-			name="viewport"
-			content="width=device-width,initial-scale=1.0,minimum-scale=1.0,maximum-scale=1.0"
-		/>
+	
+
+		@livewireStyles
 	</head>
 	<body>
-		<div class="container">
-			<div class="contact-box">
-				<div class="left"></div>
-				<div class="right">
-					<h2>Contact Us</h2>
-					<input type="text" class="field" placeholder="Your Name" />
-					<input type="text" class="field" placeholder="Your Email" />
-					<input type="text" class="field" placeholder="Phone" />
-					<textarea placeholder="Message" class="field"></textarea>
-					<button class="btn">Send</button>
-				</div>
-			</div>
-		</div>
+
+					<livewire:contact-form />
+				
+			
+
+
+		@livewireScripts
 	</body>
 </html>
