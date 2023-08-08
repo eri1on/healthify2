68faf0cf416dd8c9a6d1dfbc80e057e222690295
eri1on <et58394@ubt-uni.net>
2023-08-08 21:01:49 +0200

    contact model of  'contact'  table  created

---
+++ app/Models/Contact.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Contact extends Model
+{
+
+
+   protected $table='contact';
+    use HasFactory;
+
+    protected $fillable=[
+
+    'firstName',
+    'email',
+    'phone',
+    'message',
+    'fk_signUp_id',
+
+
+
+    ];
+
+
+}
