5890ced9db92ce33b586762fc6ef1c540d6422df
eri1on <et58394@ubt-uni.net>
2023-08-13 01:54:46 +0200

    'contact-form' View will show a success message of message has been sent successfully.

--- resources/views/livewire/contact-form.blade.php
+++ resources/views/livewire/contact-form.blade.php
@@ -1,14 +1,20 @@
+
+
+
+
 <div class="container">
-    
+ 
     <div class="contact-box">
         <div class="left"></div>
+  
         <div class="right">
+       
             <h2>Contact Us</h2>
             <form wire:submit.prevent="submitForm">
-            <input wire:model="firstname" type="text" class="field" placeholder="Your Name" />
+            <input wire:model="firstname" type="text" class="field" placeholder="Name" readonly />
             @error('firstname') <span class="error">{{ $message }}</span> @enderror
 
-            <input wire:model="email" type="text" class="field" placeholder="Your Email" />
+            <input wire:model="email" type="text" class="field" placeholder="Email" readonly />
             @error('email') <span class="error">{{ $message }}</span> @enderror
 
             <input wire:model="phone" type="text" class="field" placeholder="Phone" />
@@ -18,13 +24,21 @@
             @error('message') <span class="error">{{ $message }}</span> @enderror
 
             <button class="btn">Send</button>
-        </form>
+           
+            </form>
 
-        @if (session()->has('success'))
-    <div style="color:red; font-size:larger" class="success-message">
-        {{ session('success') }}
-    </div>
-@endif
+           @if (session()->has('success'))
+              <div style="color:red; font-size:larger" class="success-message">
+               {{ session('success') }}
+               
+              </div>
+             
+            @endif
+            <a href="/" style="font-size: smaller">Go Back To Home Page</a>
+
         </div>
     </div>
+ 
+    
 </div>
+
