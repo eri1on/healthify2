96682123c2d35b2fa46be9f66371c11eb5225ca4
eri1on <et58394@ubt-uni.net>
2023-08-16 17:41:26 +0200

    Rewrote the message list view and added an if statement to check if user is admin or superadmin in order to know users Role

--- resources/views/livewire/message-list.blade.php
+++ resources/views/livewire/message-list.blade.php
@@ -1,21 +1,29 @@
-
-
-<div>
-    @foreach ($messages as $message)
-        <div class="message">
-            <p><strong>Name:</strong> {{ $message->firstName }}</p>
-            <p><strong>Email:</strong> {{ $message->email }}</p>
-            <p><strong>Message:</strong> {{ $message->message }}</p>
-            <p><strong>signUp ID:</strong> {{ $message->fk_signUp_id }}</p>
-            
-            <button wire:click="deleteMessage({{ $message->id }})" class="btn">Delete</button>
-        </div>
-    @endforeach
-
-    @if (session()->has('success'))
-        <div style="color: red; font-size: larger" class="success-message">
-            {{ session('success') }}
-        </div>
-    @endif
+@if (isset($unauthorized) && $unauthorized)
+<div style="color: red; background-color: #fff; padding: 10px; box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.2);">
+    Unauthorized access!  <a href="/">Go back</a>
 </div>
+@else
 
+        <div>
+            @if ($messages->isEmpty())
+                <h4 style="font-style: italic; color: #252B48; text-align: center;">No Messages Here!</h4>
+            @else
+                @foreach ($messages as $message)
+                    <div class="message">
+                        <p><strong>SignUp ID:</strong> {{ $message->fk_signUp_id }}</p>
+                        <p><strong>Name:</strong> {{ $message->firstName }}</p>
+                        <p><strong>Email:</strong> {{ $message->email }}</p>
+                        <p><strong>Message:</strong> {{ $message->message }}</p>
+                        <button wire:click="deleteMessage({{ $message->id }})" class="btn">Delete</button>
+                    </div>
+                @endforeach
+            @endif
+
+            @if (session()->has('success'))
+                <div style="color: crimson; font-size: larger;" class="success-message">
+                    {{ session('success') }}
+                </div>
+            @endif
+        </div>
+   
+@endif
