f85fe973d923922a858f063b954e127f66f7701d
EK369 <ek60111@ubt-uni.net>
2023-08-23 22:20:14 +0200

    'admin-recipe-edit' view calls livewire view in order to display the form to allow admin and superadmin to edit a recipe also sends the recipe->id that livewire view accepts

---
+++ resources/views/admin-recipe-edit.blade.php
@@ -0,0 +1,25 @@
+@extends('layouts.app')
+@section('content')
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+
+    <link rel="stylesheet" href="{{ asset('css/add-recipes.css') }}">
+    @livewireStyles
+</head>
+<body>
+
+    <livewire:admin-recipe-edit :RecipeId="$recipeData->id" />
+
+
+
+
+    @livewireScripts
+</body>
+</html>
+@endsection
