599368bdcb9f5190453ccb66f41814550b468822
EK369 <ek60111@ubt-uni.net>
2023-08-23 22:22:22 +0200

    added the html structure to allow admin and superadmin to add a new recipe, while doing this submit will be prevented until 'sendData' method validations are passed

---
+++ resources/views/livewire/add-recipes.blade.php
@@ -0,0 +1,30 @@
+
+@if(session('success'))
+<div class="alert alert-success">
+    {{ session('success') }}
+</div>
+@endif
+
+@if(session('error'))
+<div class="alert alert-danger">
+    {{ session('error') }}
+</div>
+@endif
+<div class="form-container">
+    <h2>Add a New Recipe</h2>
+    <form  method="POST"  wire:submit.prevent="sendData">
+        <div class="form-group">
+            <label for="title">Title:</label>    @error('title') <span class="error" style="color:red">{{ $message }}</span> @enderror
+            <input wire:model="title"type="text" id="title" name="title" >
+        </div>
+
+        <div class="form-group">
+            <label for="description">Description:</label>   @error('description') <span class="error" style="color:red">{{ $message }}</span> @enderror
+            <textarea wire:model="description"id="description" name="description" rows="4" ></textarea>
+        </div>
+
+        <div class="form-group">
+            <button type="submit">Submit</button>
+        </div>
+    </form>
+</div>
